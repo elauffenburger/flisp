b1b75bad00f860a228d38f88240a7bed8307370e
Eric Lauffenburger <elauffenburger@gmail.com>
2017-11-10 00:39:47 -0800

    checkpoint before i do something really stupid to map

--- src/Flisp/Flisp.fs
+++ src/Flisp/Flisp.fs
@@ -2,18 +2,20 @@ module Flisp
 open System.Collections.Generic
 open System
 open System.Xml
+open System.Security.Policy
 
 type Cell = 
     | Symbol of string
     | Value of obj
     | Number of float
-    | List of Cell list
+    | Lispt of Cell list
+    | Lambda of Lambda
     | Begin
     | End
     | Whitespace
     | Procedure of Proc
-
-and ExecEnv = IDictionary<string, Cell>
+and Lambda = { parms: Cell list; body: Cell list }
+and ExecEnv = Dictionary<string, Cell>
 and ProcResult = Success of Cell | Error of string
 and Proc = delegate of Cell list * ExecEnv -> ProcResult
 
@@ -21,27 +23,7 @@ type Expression = {cells: Cell list; env: ExecEnv }
 
 let nil = ()
 
-let proc_print cells env =
-    match cells with
-    | [] -> Success (Symbol "nil")
-    | _ -> 
-        printfn "%A" cells 
-        Success (Symbol "nil")
-
-let proc_add cells env =
-    match cells with 
-    | [Number x; Number y] -> Success (Number (x+y))
-    | _ -> Error "something bad"
-
-let makeDefaultEnv() =
-    dict [
-        "nil", Value nil;
-        "print", Procedure (new Proc(proc_print));
-        "+", Procedure (new Proc(proc_add))
-    ]
-
 let newExpr cells env = { cells = cells; env = env }
-let defaultExpr cells = newExpr cells <| makeDefaultEnv()
 
 let handleProcResult result =
     match result with
@@ -59,7 +41,7 @@ let rec eval expr =
         match x with
         | Symbol sym -> resolveSymbol sym (fun cell -> [cell])
         | Number _ -> [x]
-        | List cells -> newExpr cells expr.env |> eval
+        | Lispt cells -> newExpr cells expr.env |> eval
 
     | x::xs -> 
         match x with
@@ -71,7 +53,53 @@ let rec eval expr =
 
         | Symbol sym -> resolveSymbol sym (fun cell -> eval ({ cells = cell::xs; env = expr.env }))
         | Number _ -> x :: (newExpr xs expr.env |> eval)
-        | List cells -> newExpr cells expr.env |> eval        
+        | Lispt cells -> newExpr cells expr.env |> eval        
+
+let proc_print cells env =
+    match cells with
+    | [] -> Success (Symbol "nil")
+    | _ -> 
+        printfn "%A" cells 
+        Success (Symbol "nil")
+
+let proc_add cells env =
+    match cells with 
+    | [Number x; Number y] -> Success (Number (x+y))
+    | _ -> Error "something bad"
+
+let proc_map cells (env: ExecEnv) =
+    match cells with
+    | [Symbol "lambda"; Lispt [ Symbol el; Lispt fn ]; Lispt items] ->
+        // Map over items
+        let result = items |> List.map (fun item ->
+            let newEnv = new Dictionary<string, Cell>(env)
+
+            // Add parameter to environment
+            newEnv.[el] = Symbol el
+
+            // eval fn 
+            let res = newExpr fn newEnv |> eval
+
+            match res with
+            | [x] -> x
+            | [] -> Symbol "nil"
+            | _ -> Lispt res
+        )
+        
+        Success (Lispt result)
+    | _ -> Error "incorrect signature for map"
+
+let makeDefaultEnv() =
+    let env = dict [
+        "nil", Value nil;
+        "map", Procedure (new Proc(proc_map));
+        "print", Procedure (new Proc(proc_print));
+        "+", Procedure (new Proc(proc_add))
+    ]
+
+    new ExecEnv(env)
+
+let defaultExpr cells = newExpr cells <| makeDefaultEnv()
 
 let printNum = defaultExpr [
     Symbol "print"
@@ -80,10 +108,32 @@ let printNum = defaultExpr [
 
 let addAndPrintNums = defaultExpr [
     Symbol "print"
-    List [
+    Lispt [
         Symbol "+"
         Number 3.0
         Number 5.0
+    ]
+]
+
+let mapList = defaultExpr [
+    Symbol "map"
+    Lispt [
+        Symbol "lambda"
+        Lispt [
+            Symbol "n"
+        ]
+        Lispt [
+            Lispt [
+                Symbol "print"
+                Symbol "n"
+            ]
+            Symbol "n"
+        ]
+    ]
+    Lispt [
+        Number 3.0
+        Number 5.0
+        Number 12.0
     ]
 ]
 
